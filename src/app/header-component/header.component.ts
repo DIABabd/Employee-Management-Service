ce9a84d643276c0db1487841a3c909aad45ab6e9
unknown <Abdullah@DESKTOP-VRGQJ6K.zarm.uni-bremen.de>
2025-02-25 13:04:09 +0100

    fixed logout button appearing after logging out in the home page

--- src/app/header-component/header.component.ts
+++ src/app/header-component/header.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-import {KeycloakService} from "keycloak-angular";
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from "keycloak-angular";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header-component',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isLoggedIn = false;
+
   constructor(private keycloak: KeycloakService) {}
+
+  ngOnInit(): void {
+    // Check authentication status when component initializes
+    this.updateAuthStatus();
+  }
+
+  // Method to check if user is authenticated
+  private updateAuthStatus(): void {
+    this.keycloak.isLoggedIn().then(loggedIn => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
 
   logout() {
     this.keycloak.logout('http://localhost:4200'); // Redirect to the home page after logout
